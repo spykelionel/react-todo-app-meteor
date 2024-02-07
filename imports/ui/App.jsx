db678623f08dec443c748ddb14e7bf0b46ff31ce
spykelionel <social.lionel@gmail.com>
2024-02-07 08:26:25 +0100

    refactor task component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,27 +1,18 @@
+import { useTracker } from "meteor/react-meteor-data";
 import React from "react";
+import { TasksCollection } from "../api/TasksCollection";
+import Task from "./Task";
 
-const tasks = [
-  { _id: 0, title: "My first task" },
-  { _id: 1, title: "My second task" },
-  { _id: 2, title: "My third task" },
-  { _id: 3, title: "My fourth task" },
-];
-
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Tasks tasks={tasks} />
-  </div>
-);
-
-const Task = ({ title }) => <li>{title}</li>;
-
-const Tasks = ({ tasks }) => {
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
   return (
-    <ul>
-      {tasks.map((task) => (
-        <Task key={task._id} title={task.title} />
-      ))}
-    </ul>
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} title={task.text} />
+        ))}
+      </ul>
+    </div>
   );
 };
---
+++ imports/ui/Task.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Task = ({ title }) => <li>{title}</li>;
+
+export default Task;
