48953670bccddd464dbccfc1873f6cb5718f2617
spykelionel <social.lionel@gmail.com>
2024-02-08 21:02:51 +0100

    create tasks methods

---
+++ imports/api/tasksMethods.js
@@ -0,0 +1,38 @@
+import { check } from "meteor/check";
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "./TasksCollection";
+
+Meteor.methods({
+  "tasks.insert"(text) {
+    check(text, String);
+    if (!this.userId) {
+      throw new Meteor.Error("User is not authorized");
+    }
+    TasksCollection.insert({
+      text: text.trim(),
+      createdAt: new Date(),
+      userId: this.userId,
+    });
+  },
+  "tasks.remove"(taskId) {
+    check(taskId, String);
+    if (!this.userId) {
+      throw new Meteor.Error("User is not authorized");
+    }
+    TasksCollection.remove(taskId);
+  },
+  "tasks.setIsChecked"(taskId, isChecked) {
+    check(taskId, String);
+    check(isChecked, Boolean);
+
+    if (!this.userId) {
+      throw new Meteor.Error("User is not authorized");
+    }
+
+    TasksCollection.update(taskId, {
+      $set: {
+        isChecked,
+      },
+    });
+  },
+});
